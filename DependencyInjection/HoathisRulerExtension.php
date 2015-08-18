14c7a134108dfa8c68056434b2590f8e98a4795e
Kévin Gomez <contact@kevingomez.fr>
2015-08-18 21:55:53 +0200

    Provide a hoathis.ruler service

---
+++ DependencyInjection/HoathisRulerExtension.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Hoathis\Bundle\RulerBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+/**
+ * RulerExtension.
+ *
+ * @author Kévin Gomez <contact@kevingomez.fr>
+ */
+class HoathisRulerExtension extends Extension
+{
+    /**
+     * Responds to the twig configuration parameter.
+     *
+     * @param array            $configs
+     * @param ContainerBuilder $container
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+    }
+}
---
+++ HoathisRulerBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Hoathis\Bundle\RulerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class HoathisRulerBundle extends Bundle
+{
+}
